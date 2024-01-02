26430631a9db1cfe6bbdda6d7b58fe4ee7779a29
weoamathior <weoamathior@gmail.com>
2024-01-02 10:51:28 -0600

    initial commit

---
+++ src/test/java/irish/bla/webfluxdemo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package irish.bla.webfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
