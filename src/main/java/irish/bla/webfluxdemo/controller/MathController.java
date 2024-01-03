6c695e805a7346919b1f25a84eefd3d1b67c277d
weoamathior <weoamathior@gmail.com>
2024-01-03 06:24:25 -0600

    math controller (not reactive) - using standard webmvc semantics

---
+++ src/main/java/irish/bla/webfluxdemo/controller/MathController.java
@@ -0,0 +1,30 @@
+package irish.bla.webfluxdemo.controller;
+
+import irish.bla.webfluxdemo.dto.Response;
+import irish.bla.webfluxdemo.service.MathService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("math")
+@RequiredArgsConstructor
+public class MathController {
+
+    private final MathService mathService;
+
+    @GetMapping("square/{input}")
+    public Response findSquare(@PathVariable int input) {
+        return mathService.findSquare(input);
+    }
+
+    @GetMapping("table/{input}")
+    public List<Response> multTable(@PathVariable int input) {
+        return this.mathService.multiplicationTasble(input);
+    }
+
+}
