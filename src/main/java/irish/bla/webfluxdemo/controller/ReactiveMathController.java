6a773c5abd20ea9276e5ea82aa35958238daa751
weoamathior <weoamathior@gmail.com>
2024-01-03 06:37:20 -0600

    reactive math controller

---
+++ src/main/java/irish/bla/webfluxdemo/controller/ReactiveMathController.java
@@ -0,0 +1,30 @@
+package irish.bla.webfluxdemo.controller;
+
+import irish.bla.webfluxdemo.dto.Response;
+import irish.bla.webfluxdemo.service.ReactiveMathService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("reactive-math")
+@RequiredArgsConstructor
+public class ReactiveMathController {
+    private final ReactiveMathService reactiveMathService;
+
+    @GetMapping("square/{input}")
+    public Mono<Response> findSquare(@PathVariable int input) {
+        return reactiveMathService.findSquare(input);
+    }
+
+    @GetMapping("table/{input}")
+    public Flux<Response> multTable(@PathVariable int input) {
+        return this.reactiveMathService.multTable(input);
+    }
+
+
+}
