80e1feeed0dee24b293e9d5248c32ce5f2b1f460
weoamathior <weoamathior@gmail.com>
2024-01-03 06:50:37 -0600

    exposing stream endpoint: text_event_stream_value "breaks" the list into objects as they become available

--- src/main/java/irish/bla/webfluxdemo/controller/ReactiveMathController.java
+++ src/main/java/irish/bla/webfluxdemo/controller/ReactiveMathController.java
@@ -3,6 +3,7 @@ package irish.bla.webfluxdemo.controller;
 import irish.bla.webfluxdemo.dto.Response;
 import irish.bla.webfluxdemo.service.ReactiveMathService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,9 @@ public class ReactiveMathController {
         return this.reactiveMathService.multTable(input);
     }
 
+    @GetMapping(value = "table/{input}/stream",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Response> multTableStream(@PathVariable int input) {
+        return this.reactiveMathService.multTable(input);
+    }
 
 }
