002e877f6213a729ceef079e71d23f96995446f8
weoamathior <weoamathior@gmail.com>
2024-01-05 07:12:46 -0600

    router config predicates

--- src/main/java/irish/bla/webfluxdemo/config/RouterConfig.java
+++ src/main/java/irish/bla/webfluxdemo/config/RouterConfig.java
@@ -6,10 +6,7 @@ import irish.bla.webfluxdemo.requesthandler.RequestHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
 import java.util.function.BiFunction;
@@ -21,7 +18,12 @@ public class RouterConfig {
     private final RequestHandler requestHandler;
     private RouterFunction<ServerResponse> serverResponseRouterFunction() {
         return RouterFunctions.route()
-                .GET("square/{input}", requestHandler::squareHandler)
+                // intent - 10-19 accepted
+                // curl http://localhost:8080/router/square/10
+//                .GET("square/{input}", RequestPredicates.all(), requestHandler::squareHandler) // ok
+                .GET("square/{input}", RequestPredicates.path("*/1?").or(RequestPredicates.path("*/20")), requestHandler::squareHandler) // <- boom! 404
+//                .GET("square/{input}", RequestPredicates.path("router/square/1?").or(RequestPredicates.path("router/square/20")), requestHandler::squareHandler) // ok
+//                .GET("square/{input}", req -> ServerResponse.badRequest().bodyValue("only 10-19 allowed"))
                 .GET("table/{input}", requestHandler::tableHandler)
                 .GET("table/{input}/stream", requestHandler::tableStreamHandler)
                 // curl -XPOST -H 'Content-Type: application/json'  -d '{"first":5,"second":9}' http://localhost:8080/router/multiply
