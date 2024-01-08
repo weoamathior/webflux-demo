0e81bdab4bc1ee2c7e24fe1ae8bfeb5e5ea6e386
weoamathior <weoamathior@gmail.com>
2024-01-08 06:15:51 -0600

    simple blocking test

--- src/main/java/irish/bla/webfluxdemo/dto/Response.java
+++ src/main/java/irish/bla/webfluxdemo/dto/Response.java
@@ -1,10 +1,12 @@
 package irish.bla.webfluxdemo.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 public class Response {
     private Date date = new Date();
     private int output;
--- src/test/java/irish/bla/webfluxdemo/Lecture43GetSingleResponseTest.java
+++ src/test/java/irish/bla/webfluxdemo/Lecture43GetSingleResponseTest.java
@@ -1,9 +1,23 @@
 package irish.bla.webfluxdemo;
 
+import irish.bla.webfluxdemo.dto.Response;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public class Lecture43GetSingleResponseTest extends BaseTest{
     @Autowired
     WebClient webClient;
+
+    @Test
+
+    void blockTest() {
+        Response response = webClient
+                .get()
+                .uri("reactive-math/square/{input}", 5)
+                .retrieve()
+                .bodyToMono(Response.class)
+                .block();
+        System.out.println(response);
+    }
 }
