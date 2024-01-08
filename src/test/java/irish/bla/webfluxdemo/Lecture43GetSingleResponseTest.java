d16a9df9c7625508d08de01ee157a1a0d93600ec
weoamathior <weoamathior@gmail.com>
2024-01-08 06:23:59 -0600

    with stepverifier reactor test

--- src/test/java/irish/bla/webfluxdemo/Lecture43GetSingleResponseTest.java
+++ src/test/java/irish/bla/webfluxdemo/Lecture43GetSingleResponseTest.java
@@ -4,6 +4,8 @@ import irish.bla.webfluxdemo.dto.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class Lecture43GetSingleResponseTest extends BaseTest{
     @Autowired
@@ -19,5 +21,18 @@ public class Lecture43GetSingleResponseTest extends BaseTest{
                 .bodyToMono(Response.class)
                 .block();
         System.out.println(response);
+    }
+
+    @Test
+    void stepVerifierTest() {
+        Mono<Response> monoResponse = webClient
+                .get()
+                .uri("reactive-math/square/{input}", 5)
+                .retrieve()
+                .bodyToMono(Response.class);
+
+        StepVerifier.create(monoResponse)
+                .expectNextMatches(r -> r.getOutput() == 25)
+                .verifyComplete();
     }
 }
