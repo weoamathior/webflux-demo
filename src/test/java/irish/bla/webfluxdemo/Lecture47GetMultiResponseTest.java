c1e597f8e1a468c1bcd75682df92341edd621d37
weoamathior <weoamathior@gmail.com>
2024-01-11 07:50:26 -0600

    step verifier test for flux

---
+++ src/test/java/irish/bla/webfluxdemo/Lecture47GetMultiResponseTest.java
@@ -0,0 +1,30 @@
+package irish.bla.webfluxdemo;
+
+import irish.bla.webfluxdemo.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lecture47GetMultiResponseTest extends BaseTest{
+
+    @Autowired
+    private WebClient webClient;
+
+
+    @Test
+    void stepVerifierTest() {
+        Flux<Response> monoResponse = webClient
+                .get()
+                .uri("reactive-math/table/{input}", 5)
+                .retrieve()
+                .bodyToFlux(Response.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(monoResponse)
+                .expectNextCount(10)
+                .verifyComplete();
+    }
+}
