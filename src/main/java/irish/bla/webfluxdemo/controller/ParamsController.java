b581e841125ec956e828568c72dd87db4eeb9666
weoamathior <weoamathior@gmail.com>
2024-01-12 07:36:53 -0600

    query params testing

---
+++ src/main/java/irish/bla/webfluxdemo/controller/ParamsController.java
@@ -0,0 +1,21 @@
+package irish.bla.webfluxdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+@RestController
+@RequestMapping("jobs")
+public class ParamsController {
+
+    @GetMapping("search")
+    public Flux<Integer> searchJobs(@RequestParam("count") int count, @RequestParam("page") int page) {
+
+        return Flux.just(count, page);
+    }
+
+}
