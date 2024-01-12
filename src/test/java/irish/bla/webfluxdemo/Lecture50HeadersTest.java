136df1fd9096ed478bf8ff9e9fb136d2df1c89ab
weoamathior <weoamathior@gmail.com>
2024-01-12 06:59:16 -0600

    setting headers in request

---
+++ src/test/java/irish/bla/webfluxdemo/Lecture50HeadersTest.java
@@ -0,0 +1,39 @@
+package irish.bla.webfluxdemo;
+
+import irish.bla.webfluxdemo.dto.MultiplyRequestDto;
+import irish.bla.webfluxdemo.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lecture50HeadersTest extends BaseTest{
+
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    public void headersTest() {
+        Mono<Response> responseMono = this.webClient
+                .post()
+                .uri("reactive-math/multiply")
+                .bodyValue(buildRequestDto(5, 2))
+                .headers(h -> h.set("someKey", "someVal"))
+                .retrieve()
+                .bodyToMono(Response.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(responseMono)
+                .expectNextCount(1)
+                .verifyComplete();
+
+    }
+
+    private MultiplyRequestDto buildRequestDto(int a, int b) {
+        MultiplyRequestDto multiplyRequestDto = new MultiplyRequestDto();
+        multiplyRequestDto.setFirst(a);
+        multiplyRequestDto.setSecond(b);
+        return multiplyRequestDto;
+    }
+}
