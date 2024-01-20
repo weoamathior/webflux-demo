a46fcf3b4b9899fd17a299c9b52fab09e41eb4bd
weoamathior <weoamathior@gmail.com>
2024-01-20 10:25:34 -0800

    use of webtestclient

---
+++ src/test/java/irish/bla/webfluxdemo/webtestclient/Lecture142SimpleWebTestClientTest.java
@@ -0,0 +1,35 @@
+package irish.bla.webfluxdemo.webtestclient;
+
+import irish.bla.webfluxdemo.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class Lecture142SimpleWebTestClientTest {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void stepVerifierTest() {
+        Flux<Response> monoResponse = webTestClient
+                .get()
+                .uri("/reactive-math/square/{input}", 5)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(Response.class)
+                        .getResponseBody();
+
+        StepVerifier.create(monoResponse)
+                .expectNextMatches(r -> r.getOutput() == 25)
+                .verifyComplete();
+    }
+}
